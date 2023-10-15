ada948508559367429b5496d9c5b67c275e4013c
Matthew Ferry <mjferry2016@gmail.com>
2023-10-15 01:20:37 +0100

    Make the header work

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,47 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Menu, MenuItem } from "monday-ui-react-core";
 import menu from "../img/menu.svg";
 import "./Header.css";
+import { pathMap } from "../Routes";
 
-// TODO: Make links go somewhere
-const Header = () => (
-  <header className="Header">
-    <a href="/" className="Header-text">
-      Equitimer
-    </a>
-    <a href="/">
-      <img src={menu} className="Header-menu" alt="menu" />
-    </a>
-  </header>
-);
+const Header = () => {
+  const navigate = useNavigate();
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  return (
+    <header className="Header">
+      <a href="/equitimer" className="Header-text">
+        Equitimer
+      </a>
+      <div onClick={() => setMenuOpen(!isMenuOpen)}>
+        <img src={menu} className="Header-menu" alt="menu" />
+      </div>
+      {isMenuOpen && (
+        <>
+          <div className="nav-menu-bg" onClick={() => setMenuOpen(false)} />
+          <div className="nav-menu">
+            <Menu>
+              <MenuItem
+                title="Home"
+                onClick={() => (window.location = "/equitimer")}
+              />
+              <MenuItem
+                title="Measure meeting"
+                onClick={() => navigate(pathMap.Team)}
+              />
+              <MenuItem
+                title="See source code"
+                onClick={() => {
+                  window.open("https://github.com/mferryRV/equitimer");
+                  setMenuOpen(false);
+                }}
+              />
+            </Menu>
+          </div>
+        </>
+      )}
+    </header>
+  );
+};
 
 export default Header;
